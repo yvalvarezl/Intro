dbb1e7ef547b4ef19ba7d4a17cc17e50c644bd11
Yoselin Alvarez <yvalvarezl@gmail.com>
2026-08-20 07:12:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+from PIL import Image
 
 st.title("La app de Yoselin")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write("Fácilmente puedo realizar backend y frontend")
+image = Image.open('Interfaces multimodales')
+
+texto = st.text_input('Escribe algo','Este es mi texto')
+st.write ('El texto escrito es',texto)
+
+st.subheader("Ahora usemos 2 columnas")
